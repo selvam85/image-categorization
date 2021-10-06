191eb9042cf956ba1e0a6a71fcdd6bd5feb50da4
selvam <csselvam@gmail.com>
2021-10-06 00:30:16 -0400

    Updated the code with comments

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ chooseFiles.onchange = () => {
     displayImages();
 }
 
+//Display the preview of all the selected images. 5 images per row in the table.
 function displayImages() {
     let row;
     Array.prototype.forEach.call(chooseFiles.files, function(file, index) {
@@ -45,23 +46,32 @@ function displayImages() {
 }
 
 filterImages.onclick = async () => {
+    
+    //Get the filter criteria entered by the user and split them by comma
     const filterConditionArr = filterCriteria.value.split(",").map(item => item.trim());
     console.log("Filter Condition: ", filterConditionArr);
 
+    //Get all the image elements that were created to display the selected images
     let imgElements = document.getElementsByClassName("imgToFilter");
 
+    //Check if the COCO-SSD model is loaded
     if(model) {
+        //Invoke model.detect on each image using an async function and collect the array of Promises returned
         const promises = Array.prototype.map.call(imgElements, async img => {
             const predictions = await model.detect(img);
             return predictions;
         });
     
+        //Get the o/p array of predictions for all images after waiting for all Promises to fulfill.
         const predictionsArr = await Promise.all(promises);
         console.log("Predictions Array: ", predictionsArr);
         
+        //Loop through the array of predictions (predictions for all images)
         predictionsArr.forEach(function(predictions, index) {
             let matched = false;
+            //Loop through the predictions array for a single image
             for(let prediction of predictions) {
+                //If any of the filter criteria matches with a detected object class, set matched to true
                 if(filterConditionArr.includes(prediction.class)) {
                     matched = true;
                     break;
@@ -69,10 +79,12 @@ filterImages.onclick = async () => {
             }
 
             if(matched) {
+                //Highlight the image, if there is a match between the filter criteria and the objects present in the image
                 console.log("img_" + index + " matched");
                 document.getElementById("img_" + index).classList.add("saturate");
                 document.getElementById("img_" + index).classList.remove("dim");
             } else {
+                //Shade out the images, if the filter criteria didn't match with any of the objects present in the image
                 console.log("img_" + index + " did not match");
                 document.getElementById("img_" + index).classList.add("dim");
                 document.getElementById("img_" + index).classList.remove("saturate");
